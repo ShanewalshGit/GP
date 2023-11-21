4127297bae73e1f47c4072b7be8191e06512cddd
Shane Walsh <swalsh818@gmail.com>
2023-11-21 12:39:19 +0000

    Swapped image and messed with canny thresholds

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -4,26 +4,41 @@ from matplotlib import pyplot as plt
 
 print("Hello World")
 
-imgOrig = cv2.imread('ATU.jpg')
+imgOrig = cv2.imread('keanu.jpg')
 
 nrows = 2
-ncols = 2
+ncols = 5
 
 
 imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 imgOut = cv2.GaussianBlur(imgGray,(5, 5),0)
 blur = cv2.blur(imgGray,(13, 13),0)
 
-plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(imgOrig,cv2.COLOR_BGR2RGB), cmap = 'gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols,2),plt.imshow(imgGray, cmap = 'gray')
-plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols,3),plt.imshow(imgOut, cmap = 'gray')
-plt.title('GuassianBlur'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols,4),plt.imshow(blur, cmap = 'gray')
-plt.title('blur'), plt.xticks([]), plt.yticks([])
-plt.show()
+sobelHorizontal = cv2.Sobel(imgGray,cv2.CV_64F,1,0,ksize=5) # x dir
+sobelVertical = cv2.Sobel(imgGray,cv2.CV_64F,0,1,ksize=5) # y dir
+sobelSum = cv2.addWeighted(sobelVertical, 0.5, sobelHorizontal, 0.5, 0)
 
+canny = cv2.Canny(imgGray,150,250)
+
+plt.subplot(nrows, ncols,1),plt.imshow(imgOrig, cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(cv2.cvtColor(imgOrig,cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Fixed Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,3),plt.imshow(imgGray, cmap = 'gray')
+plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,4),plt.imshow(imgOut, cmap = 'gray')
+plt.title('GuassianBlur'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,5),plt.imshow(blur, cmap = 'gray')
+plt.title('blur'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,6),plt.imshow(sobelHorizontal, cmap = 'gray')
+plt.title('sobel Horizontal'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,7),plt.imshow(sobelVertical, cmap = 'gray')
+plt.title('sobel Vertical'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,8),plt.imshow(sobelSum, cmap = 'gray')
+plt.title('Sobel Sum'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,9),plt.imshow(canny, cmap = 'gray')
+plt.title('Canny'), plt.xticks([]), plt.yticks([])
+plt.show()
 
 
 
